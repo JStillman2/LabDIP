4f04d5db62de6c4798d58237b1630db919fa753f
Jay <Nicole@Nicole-Laptop.att.net>
2014-02-10 23:54:51 -0600

    Second Commit

--- src/dip/lab2/BaggageServiceTipCalculator.java
+++ src/dip/lab2/BaggageServiceTipCalculator.java
@@ -8,31 +8,25 @@ package dip.lab2;
  *
  * @author your name goes here
  */
-public class BaggageServiceTipCalculator {
-    private static final double MIN_BILL = 0.00;
-    private static final double MAX_BILL = 100.00;
-    private static final String BILL_ENTRY_ERR =
-            "Error: bill must be between " + MIN_BILL + " and "
-            + MAX_BILL;
+public class BaggageServiceTipCalculator implements TipCalculator{
+
     private static final double GOOD_RATE = 0.20;
     private static final double FAIR_RATE = 0.15;
     private static final double POOR_RATE = 0.10;
 
     private double baseTipPerBag;
     private int bagCount;
-    public enum ServiceQuality {
-        GOOD, FAIR, POOR
-    }
     private ServiceQuality serviceQuality;
 
     public BaggageServiceTipCalculator(ServiceQuality q, int bags) {
-        this.setServiceRating(q); // perform validation
+        this.setServiceQuality(q); // perform validation
         this.setBagCount(bags);
 
         baseTipPerBag = 1.00; // set default value
     }
 
-    public double getTipForBaggeHandler() {
+    @Override
+    public double getTip() {
         double tip = 0.00; // always initialize local variables
 
         switch(serviceQuality) {
@@ -50,37 +44,31 @@ public class BaggageServiceTipCalculator {
         return tip;
     }
 
-    public final void setServiceRating(ServiceQuality q) {
-        // No need to validate because enums provide type safety!
-        serviceQuality = q;
+    public double getBaseTipPerBag() {
+        return baseTipPerBag;
     }
 
-    public ServiceQuality getServiceQuality() {
-        return serviceQuality;
+    public void setBaseTipPerBag(double baseTipPerBag) {
+        this.baseTipPerBag = baseTipPerBag;
     }
 
     public int getBagCount() {
         return bagCount;
     }
 
-    public final void setBagCount(int bagCount) {
-        if(bagCount < 0) {
-            throw new IllegalArgumentException(
-                    "bag count must be greater than or equal to zero");
-        }
+    public void setBagCount(int bagCount) {
         this.bagCount = bagCount;
     }
 
-    public double getBaseTipPerBag() {
-        return baseTipPerBag;
+    public ServiceQuality getServiceQuality() {
+        return serviceQuality;
     }
 
-    public void setBaseTipPerBag(double baseTipPerBag) {
-        if(baseTipPerBag < 0) {
-            throw new IllegalArgumentException(
-                    "error: base tip must be greater than or equal to zero");
-        }
-        this.baseTipPerBag = baseTipPerBag;
+    public void setServiceQuality(ServiceQuality serviceQuality) {
+        this.serviceQuality = serviceQuality;
     }
+
+
+
 
 }
