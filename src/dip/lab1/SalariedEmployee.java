de3296d9c62f5d0e497c4fd1d971c67ecfeeb0f4
Jay <Nicole@Nicole-Laptop.att.net>
2014-02-10 23:25:04 -0600

    First Commit

---
+++ src/dip/lab1/SalariedEmployee.java
@@ -0,0 +1,53 @@
+package dip.lab1;
+
+import java.util.logging.Logger;
+
+/**
+ * A simple implementation sub-class of Employee. These are low-level classes
+ * in the DIP. Does it meet the rules of DIP? If not fix it.
+ *
+ * @author your name goes here
+ */
+public class SalariedEmployee implements Employee {
+    
+    private double annualSalary;
+    private double annualBonus;
+    
+
+    
+
+    /**
+     * Convenience constructor. Is this the best way to go?
+     * @param annualSalary - the employee's annual salary
+     * @param annualBonus - a bonus benefit, if any
+     */
+    public SalariedEmployee(double annualSalary, double annualBonus) {
+        setAnnualSalary(annualSalary);
+        setAnnualBonus(annualBonus);
+    }
+
+    @Override
+    public double getAnnualWages() {
+        return annualSalary + annualBonus;
+        
+    
+}
+
+    public double getAnnualSalary() {
+        return annualSalary;
+    }
+
+    public void setAnnualSalary(double annualSalary) {
+        this.annualSalary = annualSalary;
+    }
+
+    public double getAnnualBonus() {
+        return annualBonus;
+    }
+
+    public void setAnnualBonus(double annualBonus) {
+        this.annualBonus = annualBonus;
+    }
+    
+}
+
